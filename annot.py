625a8ceee567e1c45ce1362c00dd79c5ac233808
yukiue <45696312+yukiue@users.noreply.github.com>
2021-01-25 01:47:48 +0900

    add annotation to pdf

--- annot.py
+++ annot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 import fitz
 
 
@@ -7,17 +9,41 @@ def add_annot(num_page, x, y, text):
     page = doc[num_page]
     size = page.MediaBoxSize
     width, height = size[0], size[1]
-    print(width, height)
+    # print(width, height)
     page.addTextAnnot((width * x, y * height), text, icon='Comment')
 
 
-def main():
-    global doc
-    doc = fitz.open('dummy.pdf')
-    add_annot(0, 0.9, 0.5, 'str')
+doc = fitz.open('dummy.pdf')
 
-    doc.save('out.pdf')
+ant = ''
+with open('dummy.ant') as f:
+    for line in f:
+        ant += line
+# print(ant)
 
+for s in re.split(r'#\+', ant):
+    if 'annotation:' in s:
+        regex = re.compile(
+            r'annotation:\s(.+)\s(.+)\s(.+)\s-(.+)\s\[(.+)\]\n(.*)',
+            re.MULTILINE | re.DOTALL)
+        mo = regex.search(s)
+        num_page = int(mo.groups()[0]) - 1
+        x = float(mo.groups()[1])
+        y = float(mo.groups()[2])
+        name = mo.groups()[3]
+        date = mo.groups()[4]
+        text = mo.groups()[5]
 
-if __name__ == "__main__":
-    main()
+        # print(num_page)
+        # print(x)
+        # print(y)
+        # print(name)
+        # print(date)
+        # print(text)
+
+        add_annot(num_page, x, y, text)
+
+doc.save('out.pdf')
+
+# if __name__ == "__main__":
+#     main()

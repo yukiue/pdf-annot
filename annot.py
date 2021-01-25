1dbaf6031b29e02e245785ab49beff18f53c09ea
yukiue <45696312+yukiue@users.noreply.github.com>
2021-01-25 10:28:01 +0900

    add description of function

--- annot.py
+++ annot.py
@@ -9,6 +9,13 @@ import fitz
 
 
 def add_annot(num_page, x, y, text):
+    '''
+    num_page: page number (zero-based indexing)
+    x: x coordinate (0.0 ~ 1.0)
+    y: y coordinate (0.0 ~ 1.0)
+    (The top-left corner is the origin.)
+    text: commentary text
+    '''
     page = doc[num_page]
     size = page.MediaBoxSize
     width, height = size[0], size[1]
